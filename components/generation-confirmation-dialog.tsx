ebd6fa2fc9e59a6d3564d49673e0b232c08bec8e
Agentscreator <agentverse884@gmail.com>
2025-12-30 18:24:25 -0500

    Simplify confirmation button text to just 'Confirm'

--- components/generation-confirmation-dialog.tsx
+++ components/generation-confirmation-dialog.tsx
@@ -96,7 +96,7 @@ export function GenerationConfirmationDialog({
             style={{ backgroundSize: '200% 100%' }}
           >
             <Sparkles className="w-5 h-5 mr-2 animate-pulse" strokeWidth={1.5} />
-            Confirm Generation
+            Confirm
           </Button>
         </div>
       </div>
